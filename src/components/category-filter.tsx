41233e53ea454cd67370aa26d9e09d2640c1d43e
Paul Rogers <paulrogers@macbookpro.lan>
2026-02-03 21:49:08 -0800

    Use Dribbble icon as Basketball substitute

--- src/components/category-filter.tsx
+++ src/components/category-filter.tsx
@@ -9,7 +9,7 @@ import {
     Utensils,
     Shovel, // Garden/Yard
     MonitorSmartphone,
-    Basketball,
+    Dribbble,
     Plane, // Travel
     Shirt, // Clothing
     Armchair, // Household
@@ -23,7 +23,7 @@ const CATEGORY_ICONS: Record<string, React.ElementType> = {
     'Kitchen': Utensils,
     'Garden/Yard': Shovel,
     'Electronics': MonitorSmartphone,
-    'Recreation': Basketball,
+    'Recreation': Dribbble,
     'Travel': Plane,
     'Clothing': Shirt,
     'Household': Armchair,
